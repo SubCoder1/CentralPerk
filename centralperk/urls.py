b7b12a8a96e6681f1a326d7c3816ae02354265de
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-03-12 00:37:56 +0530

    added view_profile path in urls

--- centralperk/urls.py
+++ centralperk/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from AUth.views import register_user, user_login, user_logout
+from Profile.views import view_profile
+#from Home.views import home_view
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,4 +12,7 @@ urlpatterns = [
     path('', user_login, name='user_login'),
     path('logout/', user_logout, name='user_logout'),
     path('register/', register_user, name='register_user'),
+    path('profile/<str:username>', view_profile, name='view_profile'),
+    #path('home/', home_view, name='home_view'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
