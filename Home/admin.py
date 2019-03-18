6bfbfcea4c73ffa9d6aa951e10d998475a4f953d
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-03-19 01:57:47 +0530

    registered post model to admin panel

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Home.models import PostModel
 
-# Register your models here.
+class PostModelAdmin(admin.ModelAdmin):
+    readonly_fields=('unique_id','post_id')
+    list_display = ('status', 'user', 'date_time',)
+
+
+admin.site.register(PostModel, PostModelAdmin)
