279c3bf32d0a01d21ec0d54ef42cf5e76eebda76
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-03-19 01:43:19 +0530

    created model for storing user posts in db

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+import uuid
+from AUth.models import User
 # Create your models here.
+
+class PostModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    unique_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post_id = models.CharField(max_length=10, editable=False, default='')
+    date_time = models.DateTimeField(blank=True)
+    status = models.CharField(max_length=500, blank=True)
+    caption = models.CharField(max_length=200, blank=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    pic = models.ImageField(upload_to='post_images', blank=True)
+    objects = models.Manager()
+
+    class Meta:
+        ordering = ('-date_time',)
+        verbose_name = 'Post'
+
+    def __str__(self):
+        if self.status:
+            return 'status ' + self.post_id 
+        else:
+            return 'pic ' + self.post_id
