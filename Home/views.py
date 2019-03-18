2d3e0323d47665dcc50cbba78472c44701800213
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-03-19 02:23:42 +0530

    home_view now handles post-upload & view

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from Home.models import PostModel
+from Home.forms import PostForm
+import datetime
 # Create your views here.
 
-def home_view(request):
-    return render(request, 'index.html', {})
+class home_view(TemplateView):
+    template_name='index.html'
+
+    def get(self, request):
+        form = PostForm()
+        posts = PostModel.objects.all()
+
+        args = { 'form':form, 'posts':posts }
+        return render(request, self.template_name, context=args)
+    
+    def post(self, request):
+        form = PostForm(request.POST or None)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.post_id = str(post.unique_id)[:8]
+            post.date_time = datetime.datetime.now()
+            post.user = request.user
+            post.save()
+
+            return redirect('/home/')
