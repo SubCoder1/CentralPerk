79e543a9c3f7a8abebb30ff9568e6844ae92ee84
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-04-18 22:03:56 +0530

    added Friends model with follow unfollow methods

--- Profile/models.py
+++ Profile/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from AUth.models import User
 # Create your models here.
+class Friends(models.Model):
+    followers = models.ManyToManyField(User)
+    current_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='current_user')
+    following = models.ManyToManyField(User, related_name='following')
+    objects = models.Manager()
+
+    @classmethod
+    def follow(cls, current_user, follow_user):
+        # current_user -> following +1
+        friend, created = cls.objects.get_or_create(current_user=current_user)
+        del created
+        friend.following.add(follow_user)
+        
+        # follow_user -> follow +1
+        friend, created = cls.objects.get_or_create(current_user=follow_user)
+        del created
+        friend.followers.add(current_user)
+
+
+    @classmethod
+    def unfollow(cls, current_user, unfollow_user):
+        # current_user -> following -1
+        friend, created = cls.objects.get_or_create(current_user=current_user)
+        del created
+        friend.following.remove(unfollow_user)
+
+        #unfollow_user -> follow -1
+        friend, created = cls.objects.get_or_create(current_user=unfollow_user)
+        del created
+        friend.followers.remove(current_user)
+
+    class Meta:
+        verbose_name = 'Friend'
