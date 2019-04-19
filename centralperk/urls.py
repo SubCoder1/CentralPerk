da2c8958b3c789eec8a47a44014eafa73a35684e
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-04-19 05:58:40 +0530

    added path to manage_relation view

--- centralperk/urls.py
+++ centralperk/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from AUth.views import register_user, user_login, user_logout
-from Profile.views import view_profile, edit_profile
+from Profile.views import view_profile, edit_profile, manage_relation
 from Home.views import home_view
 from django.conf import settings
 from django.conf.urls.static import static
@@ -14,6 +14,7 @@ urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('profile/<str:username>', view_profile, name='view_profile'),
     path('profile/<str:username>/edit', edit_profile.as_view(), name='edit_profile'),
+    path('profile/<str:username>/<str:option>', manage_relation, name='manage_relation'),
     path('home/', home_view.as_view(), name='home_view'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
