9f840d044f5e7ea0b94b4e79b098b12dca84a830
Souma Kanti Ghosh <soumakantighosh19@gmail.com>
2019-05-15 18:08:19 +0530

    lengthy posts are managed using js

--- Home/static/js/index.js
+++ Home/static/js/index.js
@@ -1,3 +1,4 @@
+// JS code for nav-bar search scroll-effect
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
@@ -8,3 +9,39 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
+
+// JS code for post-card data overflow management
+var elms = document.querySelectorAll("[id='post-data']");
+var post_container = document.querySelectorAll("[id='post-container']");
+for(var i=0; i < elms.length; i++) {
+  (function (i) {
+    var post = JSON.parse(elms[i].textContent);
+    var newline_count = (post.match(/\r\n/g) || '').length + 1;
+    if (newline_count) {
+      var lines = post.split("\r\n");
+      if (lines.length > 5) {
+        var read_less = lines.slice(0,5);
+        // var read_more = lines.slice(5,lines.length);
+        // console.log(read_less);
+        // console.log("-------");
+        // console.log(read_more);
+        for (var j=0; j < 5; j++) {
+          post_container[i].innerHTML += read_less[j] + '<br/>';
+        }
+        post_container[i].innerHTML += "<a class='body_link' href='#' style='text-decoration: none'>Read More..</a>"
+      } else {
+        var read_less = lines.slice(0,lines.length);
+        for (var j=0; j < lines.length; j++) {
+          post_container[i].innerHTML += read_less[j] + '<br/>';
+        }
+      }
+    } else if (post.length > 200) {
+      post_container[i].innerHTML += post.slice(0,200);
+      post_container[i].innerHTML += "<a class='body_link' href='#' style='text-decoration: none'>Read More..</a>"
+    } 
+    else {
+      post_container[i].innerHTML += post;
+    }
+  }).call(this, i);
+}
+  
