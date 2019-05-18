e480fd4efaa8c29be78678854a71710882f6f9be
Souma Kanti Ghosh <soumakantighosh19@gmail.com>
2019-05-18 13:47:21 +0530

    handled corner case and commented profile.js

--- Profile/static/js/profile.js
+++ Profile/static/js/profile.js
@@ -32,13 +32,20 @@ function get_user_activity() {
             // Using complete in AJAX to check whether the previous request successfully executed or not. 
             // console.log(response.responseJSON);  // log the returned json to the console
             render = document.getElementById('id_activity');
-            if (response.responseJSON == "online") {
+            if (!response.responseJSON) {
+                // Case 1 -- corner case when the activity of the user to be accquired is not being followed.
+                // In that case, do nothing and stop the ajax requests being sent to the server every second.
+            }
+            else if (response.responseJSON == "online") {
+                // Case 2 -- The user activity to be checked is being followed and is online ATM.
                 render.innerHTML = "<h3><i class='material-icons' style='color:#adff2f; font-size:8px'>lens</i></h3>";
                 render.innerHTML += "<h4 style='opacity: 0.5'>Activity</h4>";
                 setTimeout(get_user_activity, 1000);
             } else if (response.responseJSON == "#") {
-            
+                // Case 3 -- The user is in his/her own profile page.
+                // In that case, No need to check continuously and show logged-in user activity.
             } else {
+                // Case 4 -- The user activity to be checked is being followed but isn't online ATM.
                 render.innerHTML = "<h3>" + response.responseJSON + "</h3>";
                 render.innerHTML += "<h4 style='opacity: 0.5'>Activity</h4>";
                 setTimeout(get_user_activity, 1000);
