fc7bad6450804286eed5960beb668a7d9dd8a411
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-05-27 00:33:50 +0530

    changed PostModelAdmin view

--- Home/admin.py
+++ Home/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from Home.models import PostModel
 
 class PostModelAdmin(admin.ModelAdmin):
-    readonly_fields=('unique_id','post_id')
-    list_display = ('post_id', 'user', 'date_time',)
+    readonly_fields = ('unique_id','post_id')
+    fieldsets = (
+        ( 'Posted By', {'fields' : ('user',)} ),
+        ( 'ID', {'fields' : ('post_id', 'unique_id',)} ),
+        ( 'Post', {'fields' : ('status', 'caption', 'pic', 'date_time', 'location', )} ),
+        ( 'Send_to', {'fields' : ('send_to', )} ),
+    )
 
 
 admin.site.register(PostModel, PostModelAdmin)
