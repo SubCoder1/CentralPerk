ba0e3e4fae55c41094fe10ae7272244198e86341
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-05-27 09:11:02 +0530

    added celery config in settings.py

--- centralperk/settings.py
+++ centralperk/settings.py
@@ -103,6 +103,13 @@ CACHES = {
     }
 }
 
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Kolkata'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
