a2c286aa6db76b16bcde9de734c04049e800bd34
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-05-31 02:37:30 +0530

    user activity is fixed bug in last_activity.py

--- Profile/last_activity.py
+++ Profile/last_activity.py
@@ -4,22 +4,28 @@ import pytz
 
 def activity(log):
     tz = pytz.timezone('Asia/Kolkata')
-    time_now = datetime.datetime.now().astimezone(tz=tz)
 
-    duration = time_now - log
+    duration = datetime.datetime.now().astimezone(tz) - log.astimezone(tz)
+    print(duration)
     days = duration.days
     mins = round(duration.seconds / 60)
     hours = round(mins / 60)
 
+    print(f"Hours = {hours}, mins = {mins}")
+
     if days:
         result = f"{days} days ago"
     elif hours:
-        result = f"{hours} minutes ago"
+        if hours > 1:
+            result = f"{hours} hours ago"
+        else:
+            result = f"{hours} hour ago"
     elif mins:
         result = f"{mins} minutes ago"
     else:
         result = "seconds ago"
     
+    print(result)
     return result
     
     
