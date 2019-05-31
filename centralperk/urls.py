d75cb70117683d084d4be854e6b6938a87fdc7ce
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-01 00:41:37 +0530

    updated urls.py

--- centralperk/urls.py
+++ centralperk/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import include, path
 from AUth.views import register_user, user_login, user_logout
 from Profile.views import view_profile, edit_profile, manage_relation
-from Home.views import home_view, manage_likes
+from Home.views import home_view
+from Home.views import manage_home_post_likes as manage_likes_home
+from Profile.views import manage_profile_post_likes as manage_likes_profile
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -14,9 +16,10 @@ urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('profile/<str:username>', view_profile, name='view_profile'),
     path('profile/<str:username>/edit', edit_profile.as_view(), name='edit_profile'),
+    path('profile/<str:username>/<str:post_id>/like', manage_likes_profile, name='post_like_profile'),
     path('profile/<str:username>/<str:option>', manage_relation, name='manage_relation'),
     path('home/', home_view.as_view(), name='home_view'),
-    path('home/<str:post_id>/like', manage_likes, name='post_like'),
+    path('home/<str:post_id>/like', manage_likes_home, name='post_like_home'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
