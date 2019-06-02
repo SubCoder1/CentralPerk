c2dc6bc619ca24de19adecd12b7de7dd1d05c59f
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-03 01:08:01 +0530

    updated Home.views

--- Home/views.py
+++ Home/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from Home.models import PostModel
 from Home.forms import PostForm
-from Profile.models import Friends
-from Home.tasks import share_posts
-from Profile.models import User
+from Profile.models import Friends, User
+from Home.tasks import share_posts, send_notifications
+from datetime import datetime
+import pytz
 # Create your views here.
 
 class home_view(TemplateView):
@@ -24,9 +25,14 @@ class home_view(TemplateView):
             post.user = request.user
             post.save()
             post.send_to.add(request.user)
-            share_posts.delay(request.user.username, post.post_id)   # Celery handling the task to share the post to user's followers
+            share_posts.delay(request.user.username, post.post_id)  # Celery handling the task to share the post to user's followers
             return redirect('/home/')
 
 def manage_home_post_likes(request, post_id):
-    if PostModel.objects.likes_handler(request.user.username, post_id):
-        return redirect('/home/')
+    tz = pytz.timezone('Asia/Kolkata')
+    now = datetime.now().astimezone(tz)
+
+    if PostModel.objects.likes_handler(request.user.username, post_id) == 'Liked':
+        # Notify the user whose post is being liked
+        send_notifications.delay(username=request.user.username, reaction="Liked", date_time=now, post_id=post_id)
+    return redirect('/home/')
