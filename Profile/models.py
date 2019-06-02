a9bcd3626b1e37a32bb0b9f17b86578668c0125a
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-02 20:54:24 +0530

    added default image to profile_pic

--- Profile/models.py
+++ Profile/models.py
@@ -15,7 +15,7 @@ class User(AbstractBaseUser):
     gender = models.CharField(max_length=20, choices=GENDER)
     email = models.EmailField(max_length=255,unique=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', default='/profile_pics/default.png')
     admin = models.BooleanField(default=False) # a superuser
     staff = models.BooleanField(default=False) # a admin user; non super-user
     active = models.BooleanField(default=True)
