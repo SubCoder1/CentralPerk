4456208285382e973bc728e20c431c910e927493
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-02 20:32:48 +0530

    updated Profile.admin

--- Profile/admin.py
+++ Profile/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
-from Profile.models import Friends
-# Register your models here.
+from Profile.models import User, Friends
+from Profile.forms import UserAdminChangeForm, Registerform
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 
+# Define your model Admins here.
+class UserAdmin(BaseUserAdmin):
+    change_form = UserAdminChangeForm
+    add_form = Registerform
+
+    list_display = ('username', 'email', 'active')
+    list_filter = ('admin', 'staff', 'active')
+    fieldsets = (
+        ( None, {'fields' : ('email', 'password')} ),
+        ( 'Personal Info', {'fields' : ('full_name', 'birthdate', 'gender', 'username', 'bio', 'profile_pic')} ),
+        ( 'Permissions', {'fields' : ('admin', 'staff', 'active')} ),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password',)}
+        ),
+    )
+    search_fields = ('email', 'username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+# Register your models here.
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+admin.site.site_header = 'DJokerAdmin'
 admin.site.register(Friends)
