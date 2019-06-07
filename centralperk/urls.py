83ef8042943662285c42c21e165d59b0d1c48775
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-07 10:27:27 +0530

    updated urls.py

--- centralperk/urls.py
+++ centralperk/urls.py
@@ -1,8 +1,10 @@
-
 from django.contrib import admin
 from django.urls import include, path
 from AUth.views import register_user, user_login, user_logout
-from Profile.views import view_profile, edit_profile, manage_relation, del_user_post
+from Profile.views import (
+    view_profile, edit_profile, 
+    manage_relation, del_user_post,
+    post_view )
 from Home.views import home_view, clear_all_notification
 from Home.views import manage_home_post_likes as manage_likes_home
 from Profile.views import manage_profile_post_likes as manage_likes_profile
@@ -16,6 +18,7 @@ urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('profile/<str:username>', view_profile, name='view_profile'),
     path('profile/<str:username>/edit', edit_profile.as_view(), name='edit_profile'),
+    path('profile/<str:post_id>/view', post_view, name='view_post'),
     path('profile/<str:username>/<str:post_id>/like', manage_likes_profile, name='post_like_profile'),
     path('profile/<str:post_id>/del', del_user_post, name='del_user_post'),
     path('profile/<str:username>/<str:option>', manage_relation, name='manage_relation'),
