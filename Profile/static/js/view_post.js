72fbcc03506eb074514e6f826def146a84413e8b
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-10 23:41:39 +0530

    del view_post.js

--- Profile/static/js/view_post.js
+++
@@ -1,4 +0,0 @@
-// JS code to make post-data-card
-const post_img = document.getElementById('post-img').clientHeight;
-var post_data = document.getElementById('post-data');
-post_data.style.height = post_img + "px";
