0adb8f827953e587ca256c53def652ee3ba70da8
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-13 02:07:22 +0530

    added CommentForm in Home.forms

--- Home/forms.py
+++ Home/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from Home.models import PostModel
+from Home.models import PostModel, PostComments
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = PostModel
         fields = ('status_caption', 'pic', 'location')
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = PostComments
+        fields = ('comment',)
