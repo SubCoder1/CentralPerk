8aaa6a60df4e8a4216649f21d66f5e77f45913cb
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-12 19:06:07 +0530

    added PostComments model in Home.models

--- Home/models.py
+++ Home/models.py
@@ -24,7 +24,7 @@ class PostModel(models.Model):
     likes_count = models.PositiveIntegerField(default=0)
     unique_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     post_id = models.CharField(max_length=10, editable=False, default='')
-    date_time = models.DateTimeField(blank=True)
+    date_time = models.DateTimeField(auto_now_add=True)
     status_caption = models.CharField(max_length=500, blank=True)
     location = models.CharField(max_length=200, blank=True)
     pic = models.ImageField(upload_to=user_directory_path, blank=True)
@@ -50,30 +50,47 @@ def submission_delete(sender, instance, **kwargs):
 class PostLikes(models.Model):
     post_obj = models.OneToOneField(PostModel, on_delete=models.CASCADE, default=1, related_name='post_like_obj')
     likes = models.ManyToManyField(User, related_name='likes', default=1)
+    date_time = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Post_Likes'
 
     def __str__(self):
         return "post object - " + str(self.pk)
 
+class PostComments(models.Model):
+    post_obj = models.ForeignKey(PostModel, on_delete=models.CASCADE, default=1, related_name='post_cmmnt_obj')
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='by')
+    comment = models.TextField(max_length=500, blank=False)
+    reply = models.ForeignKey('PostComments', on_delete=models.SET_NULL, blank=True, null=True, related_name='replies')
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Post_Comments'
+
+    def __str__(self):
+        return "comment object - " + str(self.pk)
+
 REACTION = ( ('Liked', 'Liked'), ('Commented', 'Commented'), ('Sent Follow Request', 'Sent Follow Request'), )
 
 class UserNotification(models.Model):
-    user_to_notify = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1, related_name='notifications', verbose_name="to_notify")
+    user_to_notify = models.ForeignKey(User, on_delete=models.CASCADE, default=1, related_name='notifications', verbose_name="to_notify")
     # User who liked/commented 'to_notify's post or send him/her a follow request
     poked_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1, blank=False, verbose_name="reacting_user")
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name="post", null=True, blank=True)
-    date_time = models.DateTimeField(blank=True)
+    date_time = models.DateTimeField(auto_now_add=True)
     reaction = models.CharField(max_length=20, choices=REACTION)
     objects = models.Manager()
 
     class Meta:
         ordering = ('-date_time',)
 
-    @classmethod
-    def create_notify_obj(cls, to_notify, by, reaction, date_time, post_obj=None):
-        poked_user = User.objects.get(username=by)
-        obj = cls.objects.create(user_to_notify=to_notify, poked_by=poked_user, post=post_obj, date_time=date_time, reaction=reaction)
-        return obj
-
     def __str__(self):
         return self.pk
+
+    @classmethod
+    def create_notify_obj(cls, to_notify, by, reaction, post_obj=None):
+        poked_user = User.objects.get(username=by)
+        obj = cls.objects.create(user_to_notify=to_notify, poked_by=poked_user, post=post_obj, reaction=reaction)
+        return obj
