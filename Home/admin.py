c83274c66c0df8eb16a74756c28a83cbd65b4b53
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-15 00:42:16 +0530

    added PostComments model admin

--- Home/admin.py
+++ Home/admin.py
@@ -2,13 +2,26 @@ from django.contrib import admin
 from Home.models import PostModel, PostLikes, PostComments, UserNotification
 
 class PostModelAdmin(admin.ModelAdmin):
-    readonly_fields = ('unique_id','post_id')
+    list_display = ('unique_id', 'location', 'user', 'date_time', )
+    readonly_fields = ('unique_id','post_id', 'likes_count')
     fieldsets = (
         ( 'Posted By', {'fields' : ('user',)} ),
         ( 'ID', {'fields' : ('post_id', 'unique_id',)} ),
-        ( 'Post', {'fields' : ('status', 'caption', 'pic', 'date_time', 'location', )} ),
+        ( 'Post', {'fields' : ('status_caption', 'pic', 'location', )} ),
         ( 'Send_to', {'fields' : ('send_to', )} ),
-        ( 'Likes', {'fields' : ('likes_count', 'likes')} ),
+        ( 'Likes', {'fields' : ('likes_count',)} ),
+    )
+
+class PostCommentsAdmin(admin.ModelAdmin):
+    list_display = ('post_obj', 'user', 'date_time')
+    readonly_fields = ('post_id', 'date_time',)
+    fieldsets = (
+        ( 'Commented by', {'fields' : ('user',)} ),
+        ( 'Post_ID', {'fields' : ('post_id',)} ),
+        ( 'Commented on', {'fields' : ('post_obj', )} ),
+        ( 'Comment', {'fields' : ('comment',)} ),
+        ( 'Date_time', {'fields' : ('date_time',)} ),
+        ( 'Replies', {'fields' : ('reply',)} ),
     )
 
 class UserNotificationAdmin(admin.ModelAdmin):
@@ -20,4 +33,4 @@ class UserNotificationAdmin(admin.ModelAdmin):
 admin.site.register(PostModel, PostModelAdmin)
 admin.site.register(UserNotification, UserNotificationAdmin)
 admin.site.register(PostLikes)
-admin.site.register(PostComments)
+admin.site.register(PostComments, PostCommentsAdmin)
