798954e852511e6f6f99916973974079e151edbb
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-16 12:04:51 +0530

    added JS code for handling comment replies

--- Profile/static/js/view_post.js
+++ Profile/static/js/view_post.js
@@ -5,7 +5,31 @@ const card_footer_height = 53;
 
 var value = card_height - (card_header_height + card_footer_height) - 10;
 var attribute = "height:" + value.toString() + "px";
-console.log(attribute);
+//console.log(attribute);
 
 var comment_body_height = document.getElementById('id_view-post-comment-body');
 comment_body_height.setAttribute('style', attribute);
+
+
+//JS code for handling comment replies
+var length = 0;
+var replies = document.querySelectorAll('.comment-reply');
+replies.forEach(function(reply){
+    reply.addEventListener("click", function() {
+        document.querySelector(".reply-to").value = reply.id;
+        var value = "@" + reply.parentElement.querySelector('.m-0').getElementsByTagName('a')[0].innerText + "";
+        document.getElementById('comment_box').value = value;
+        length = value.length;
+        document.getElementById('comment_box').focus();
+    });
+});
+
+var comment_textbox = document.getElementById('comment_box');
+comment_textbox.addEventListener('input', function() {
+    if (comment_textbox.value.length < length-1) {
+        document.querySelector(".reply-to").value = "";
+    }
+    else if (comment_textbox.value == "") {
+        document.querySelector(".reply-to").value = "";
+    }
+});
