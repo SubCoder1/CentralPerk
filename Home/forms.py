9ef8550bda30aaf8ee07c341e73a3b07f49617f8
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-06-19 01:51:54 +0530

    added validations for comment & posts from home

--- Home/forms.py
+++ Home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from Home.models import PostModel, PostComments
 
 class PostForm(forms.ModelForm):
@@ -6,9 +7,26 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = PostModel
         fields = ('status_caption', 'pic', 'location')
+    
+    def clean(self):
+        status_caption = str(self.cleaned_data.get('status_caption'))
+        if status_caption.isspace() or len(status_caption) < 1: # status_caption field is empty
+            if not self.cleaned_data.get('pic'):    # No pic uploaded
+                # No pic uploaded + no status means empty post... Throw an error
+                self.add_error('status_caption', 'cannot be empty')
+                self.add_error('pic', 'cannot be blank')
+
+        return self.cleaned_data
 
 class CommentForm(forms.ModelForm):
 
     class Meta:
         model = PostComments
         fields = ('comment',)
+
+    def clean_comment(self):
+        comment = str(self.cleaned_data.get('comment'))
+        if comment.isspace() or not len(comment):
+            # Cannot accept blank comments or comments with only spaces or newlines
+            self.add_error('comment', 'cannot be empty')
+        return comment
