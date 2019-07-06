0b57e6507826674dca7cebfb01f8626a2cbdfaf6
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-07-07 00:34:01 +0530

    updated signup main.js to handle async validations

--- AUth/static/signup/js/main.js
+++ AUth/static/signup/js/main.js
@@ -1,70 +1,202 @@
+$('document').ready(function() {
+    // login-loading-gif remains hidden till login-btn is clicked
+    $('#loading-gif').attr('hidden', 'true');
 
-(function ($) {
-    "use strict";
-
-
-    /*==================================================================
-    [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
+    // JS Function to accquire the csrftoken
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
             }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
-    [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var message = $('.validate-input textarea[name="message"]');
-
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
         }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
 
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
+    var username_error = $('#form-error-username');
+    var register_username = $('#register-username');
+    register_username.change(function(event) {
+        event.preventDefault();
+        if(/^[a-zA-Z0-9- ]*$/.test(register_username.val()) == false) {
+            register_username.css('border-bottom', '2.5px solid #fc581b');
+            username_error.text("Username should only contain letters & numbers");
         }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
+        else if (register_username.val().length < 5) {
+            register_username.css('border-bottom', '2.5px solid #fc581b');
+            username_error.text("Username should be > 6 characters");
+        } else if (register_username.val().length > 20) {
+            register_username.css('border-bottom', '2.5px solid #fc581b');
+            username_error.text("Username should be < 21 characters");
+        } else if (register_username.val().indexOf(' ') > -1) {
+            register_username.css('border-bottom', '2.5px solid #fc581b');
+            username_error.text("Username should consist only characters");
         }
+        else {
+            $.ajax({
+                url : '',
+                type : 'POST',
+                data : {
+                    csrfmiddlewaretoken : csrftoken,
+                    username : register_username.val(),
+                    activity : 'check username validity',
+                },
 
-        return check;
+                complete : function(response) {
+                    if (response.responseJSON == 'valid username') {
+                        register_username.css('border-bottom', '2.5px solid springgreen');
+                        username_error.text("");
+                    } else {
+                        register_username.css('border-bottom', '2.5px solid #fc581b');
+                        username_error.text(response.responseJSON);
+                    }
+                }
+            });
+        }
     });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
+    var fullname_error = $('#form-error-fullname');
+    var register_fullname = $('#register-fullname');
+    register_fullname.change(function(event) {
+        event.preventDefault();
+        if (/^[a-zA-Z0-9- ]*$/.test(register_fullname.val()) == false || register_fullname.val().length == 0 || /\d/.test(register_fullname.val())) {
+            register_fullname.css('border-bottom', '2.5px solid #fc581b');
+            fullname_error.text("Full name should contain only letters");
+        } else if (register_fullname.val().length > 50) {
+            register_fullname.css('border-bottom', '2.5px solid #fc581b');
+            fullname_error.text("Full name should be < 51 characters");
+        }
+        else {
+            register_fullname.css('border-bottom', '2.5px solid springgreen');
+            fullname_error.text("");
+        }
     });
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+    var email_error = $('#form-error-email');
+    var register_email = $('#register-email');
+    register_email.change(function(event) {
+        event.preventDefault();
+        if (register_email.val().length > 254) {
+            register_email.css('border-bottom', '2.5px solid #fc581b');
+            email_error.text("Email should be < 255 characters");
+        } else {
+            $.ajax({
+                url : '',
+                type : 'POST',
+                data : {
+                    csrfmiddlewaretoken : csrftoken,
+                    email : register_email.val(),
+                    activity : 'check email validity',
+                },
 
-        $(thisAlert).addClass('alert-validate');
-    }
+                complete : function(response) {
+                    if (response.responseJSON == 'valid email') {
+                        register_email.css('border-bottom', '2.5px solid springgreen');
+                        email_error.text("");
+                    } else {
+                        register_email.css('border-bottom', '2.5px solid #fc581b');
+                        email_error.text(response.responseJSON);
+                    }
+                }
+            });
+        }
+    });
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+    var register_gender = $('#register-gender');
+    var gender_error = $('#form-error-gender');
+    register_gender.change(function() {
+        register_gender.css('border-bottom', '2.5px solid springgreen');
+        gender_error.text("");
+    });
 
-        $(thisAlert).removeClass('alert-validate');
+    var pass_error = $('#form-error-password');
+    var register_pass = $('#register-pass');
+    register_pass.change(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url : '',
+            type : 'POST',
+            data : {
+                csrfmiddlewaretoken : csrftoken,
+                username : register_username.val(),
+                email : register_email.val(),
+                password : register_pass.val(),
+                activity : 'check pass strength',
+            },
+
+            complete : function(response) {
+                var strength = response.responseJSON;
+                if (strength.includes('medium')) {
+                    register_pass.css('border-bottom', '2.5px solid orange');
+                    pass_error.text(strength);
+                } else if (strength.includes('bad')) {
+                    register_pass.css('border-bottom', '2.5px solid #fc581b');
+                    pass_error.text(strength);
+                } else {
+                    register_pass.css('border-bottom', '2.5px solid springgreen');
+                    pass_error.text("");
+                }
+            }
+        });
+    });
+
+    var signup_btn = document.getElementById('signup-btn');
+    var togglebutton = function() {
+        $('#signup-btn').attr('title', '');
     }
     
-    
+    signup_btn.addEventListener('click', function(event) {
+        $('#loading-gif').removeAttr('hidden');
+        event.preventDefault();
+        $.ajax({
+            url : '',
+            type : 'POST',
+            beforeSend : togglebutton,
+            data : {
+                csrfmiddlewaretoken : csrftoken,
+                activity : 'register user',
+                username : register_username.val(),
+                email : register_email.val(),
+                full_name : register_fullname.val(),
+                gender : $('#choice').val(),
+                password : register_pass.val(),
+            },
+            
+            complete : function(response) {
+                $('#loading-gif').attr('hidden', 'true');
+                var status = response.responseJSON;
+                if (status['username']) {
+                    register_username.css('border-bottom', '2.5px solid #fc581b');
+                    username_error.text(status['username']);
+                } if (status['full_name']) {
+                    register_fullname.css('border-bottom', '2.5px solid #fc581b');
+                    fullname_error.text(status['full_name']);
+                } if (status['email']) {
+                    register_email.css('border-bottom', '2.5px solid #fc581b');
+                    email_error.text(status['email']);
+                } if (status['gender']) {
+                    register_gender.css('border-bottom', '2.5px solid #fc581b');
+                    gender_error.text(status['gender']);
+                } if(status['password']) {
+                    status = String(status['password'])
+                    if (status.includes('bad') || status == 'This field is required.') {
+                        register_pass.css('border-bottom', '2.5px solid #fc581b');
+                    } else if (status.includes('medium')) {
+                        register_pass.css('border-bottom', '2.5px solid orange');
+                    } else {
+                        register_pass.css('border-bottom', '2.5px solid springgreen');
+                    }
+                    pass_error.text(status);
+                } if (status == 'valid form') {
+                    window.location.href += 'home/';
+                }
+            }
 
-})(jQuery);
+        });
+    });
+});
