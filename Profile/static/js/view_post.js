d82aa9a7d35c103e0cdfbb00742b1cf9369238a4
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-07-23 00:41:23 +0530

    updated view_post.js

--- Profile/static/js/view_post.js
+++ Profile/static/js/view_post.js
@@ -6,17 +6,19 @@ if (post_card) {
     const card_footer_height = 53;
     const like_card_footer_height = 63;
 
-
     var comment_body_height = card_height - (card_header_height + card_footer_height) - 4;
     var like_body_height = card_height - (card_header_height + like_card_footer_height);
     var comment_height = "height:" + comment_body_height.toString() + "px";
     var like_height = "height:" + like_body_height.toString() + "px";
-    //console.log(attribute);
+    var details_height = "height:" + (comment_body_height + 37).toString() + "px";
 
     var comment_body_height = document.getElementById('id_view-post-comment-body');
     var like_body_height = document.getElementById('id_view-post-like-body');
+    var details_body_height = document.getElementById('id_view-post-details-body');
     comment_body_height.setAttribute('style', comment_height);
     like_body_height.setAttribute('style', like_height);
+    details_body_height.setAttribute('style', details_height);
+
 };
 
 // JS code for handling comment replies
