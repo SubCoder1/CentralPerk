5616195b83792e479eb7e5f1263bd4d0b4d9dd73
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-07-30 03:36:54 +0530

    updated urls.py as per changed view names

--- centralperk/urls.py
+++ centralperk/urls.py
@@ -4,10 +4,9 @@ from AUth.views import register_user, user_login, user_logout
 from Profile.views import (
     view_profile, edit_profile, 
     manage_relation, del_user_post,
-    post_view )
+    post_view, manage_post_likes )
 from Home.views import home_view, clear_all_notification
 from Home.views import manage_home_post_likes as manage_likes_home
-from Profile.views import manage_profile_post_likes as manage_likes_profile
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,8 +27,7 @@ Profile = [
     path('profile/<str:username>', view_profile, name='view_profile'),
     path('profile/<str:username>/edit', edit_profile.as_view(), name='edit_profile'),
     path('profile/<str:post_id>/view', post_view, name='view_post'),
-    path('profile/<str:post_id>/like/<str:view_post>', manage_likes_profile, name='like_from_post_view'),
-    path('profile/<str:post_id>/like', manage_likes_profile, name='like_from_profile_post'),
+    path('profile/<str:post_id>/like', manage_post_likes, name='like_from_post_view'),
     path('profile/<str:post_id>/del', del_user_post, name='del_user_post'),
     path('profile/<str:username>/<str:option>', manage_relation, name='manage_relation'),
 ]
