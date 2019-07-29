e2941f6bfd7e8594821c4450ecc568508fa37fa3
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-07-30 03:35:59 +0530

    added async comment_send, refresh post_contents

--- Profile/static/js/view_post.js
+++ Profile/static/js/view_post.js
@@ -1,3 +1,22 @@
+// Function to accquire the csrftoken
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
 // JS code to set height of comment-view-card-body(comment box in view_post) & like-view-card-body
 const post_card = document.getElementById('id_view-post-card');
 if (post_card) {
@@ -23,25 +42,22 @@ if (post_card) {
 
 // JS code for handling comment replies
 var length = 0;
-var replies = document.querySelectorAll('.comment-reply');
-replies.forEach(function(reply){
-    reply.addEventListener("click", function() {
-        var value = reply.parentElement.querySelector('.m-0').getElementsByTagName('a')[0].innerText;
-        document.querySelector(".reply-to").value = reply.id + "_" + value;
-        //console.log(document.querySelector(".reply-to").value);
-        document.getElementById('comment_box').value = "@" + value + "";
-        length = value.length;
-        document.getElementById('comment_box').focus();
-    });
+var $comment_box = $('#comment_box');
+var $post_comment_wrapper = $('#post-comments-wrapper');
+$post_comment_wrapper.on("click", ".comment-reply", function() {
+    var reply_to = $(this).siblings('.m-0').children('a').text().replace(/\s+/g, '');
+    $(".reply-to").val($(this).attr('id') + "_" + reply_to);
+    $comment_box.val("@" + reply_to + " ");
+    length = reply_to.length;
+    $comment_box.focus();
 });
 // JS code that removes comment-reply-link if reply-to-username is removed from comment-box
-var comment_textbox = document.getElementById('comment_box');
-comment_textbox.addEventListener('input', function() {
-    if (comment_textbox.value.length < length-1) {
-        document.querySelector(".reply-to").value = "";
+$comment_box.on('input', function() {
+    if ($(this).val().length < length+1) {
+        $(".reply-to").val("");
     }
-    else if (comment_textbox.value == "") {
-        document.querySelector(".reply-to").value = "";
+    else if ($(this).val() == "") {
+        $(".reply-to").val("");
     }
 });
 
@@ -56,3 +72,144 @@ if (newline_count) {
         post_status_container.innerHTML += lines[j] + '<br/>';
       }
 }
+
+$(document).ready(function() {
+    // Refresh content on refresh-btn click
+    var $refresh_content_loading_gif = $('#loading-gif');
+    var $refresh_btn = $('#refresh-content');
+    var $indicator = $('.indicator');
+    var $success = $('#success-msg');
+    var $error = $('#error-msg');
+    $refresh_btn.on('click', function(event) {
+        event.preventDefault();
+        $refresh_content_loading_gif.removeClass('hidden');
+        // One request for refreshed-comments
+        $.ajax({
+            url : "",
+            type : "POST",
+            data : {
+                csrfmiddlewaretoken : csrftoken,
+                activity : "refresh comments",
+            },
+            complete : function(response) {
+                $post_comment_wrapper.html(response.responseText);
+            }, 
+            error : function() {
+                $indicator.toggleClass("notif-active");
+                $error.text("Refresh failed, Try again!");
+                $error.removeClass("hidden");
+                setTimeout(function(){
+                    $indicator.removeClass("notif-active");
+                    $error.text("");
+                    $error.toggleClass("hidden");
+                }, 3000);
+            }
+        });
+        // One request for refreshed-likes
+        $.ajax({
+            url : "",
+            type : "POST",
+            data : {
+                csrfmiddlewaretoken : csrftoken,
+                activity : "refresh likes",
+            },
+            complete : function(response) {
+                $indicator.toggleClass("notif-active");
+                $success.text("Successfully refreshed!");
+                $success.removeClass("hidden");
+                setTimeout(function(){
+                    $indicator.removeClass("notif-active");
+                    $success.text("");
+                    $success.toggleClass("hidden");
+                }, 2000);
+                $refresh_content_loading_gif.toggleClass('hidden');
+                $('#post-likes-wrapper').html(response.responseText);
+            }, 
+            error : function() {
+                $indicator.toggleClass("notif-active");
+                $error.text("Refresh failed, Try again!");
+                $error.removeClass("hidden");
+                setTimeout(function(){
+                    $indicator.removeClass("notif-active");
+                    $error.text("");
+                    $error.toggleClass("hidden");
+                }, 3000);
+            }
+        });
+    });
+
+    // show/hide comment replies toggle
+    var $comment_replies = $('#c-replies-collapse');
+    var $c_replies_arrow = $('.fa-angle-down');
+    $comment_replies.on('click', function() {
+        if ($comment_replies.html().indexOf("hide replies") != -1) {
+            $comment_replies.html("show replies <i class='fas fa-angle-down'></i>");
+            $(this).children($c_replies_arrow).removeClass('rotate_180');
+        } else {
+            $comment_replies.html("hide replies <i class='fas fa-angle-down'></i>");
+            $(this).children($c_replies_arrow).toggleClass('rotate_180');
+        }
+    });
+
+    // handle comment_send
+    var $comment_send_form = $('#comment_form');
+    var $comment_box = $('#comment_box');
+    var $comment_body = $('#id_view-post-comment-body');
+    $comment_send_form.on('submit', function(event) {
+        event.preventDefault();
+        $comment_body.css({"opacity":"0.7", "pointer-events":"none"});
+        $comment_send_form.css("pointer-events", "none");
+        // send comment response to server
+        $.ajax({
+            url : '',
+            type : 'POST',
+            data : {
+                csrfmiddlewaretoken : csrftoken,
+                comment : $comment_box.val(),
+                reply : $('#reply').val(),
+                activity: "add comment",
+            },
+            complete : function(response) { 
+                $comment_box.val("");
+                
+                if (response.responseJSON == 'ready to update') {
+                    // another request to update post_comments
+                    $.ajax({
+                        url : "",
+                        type : "POST",
+                        data : {
+                            csrfmiddlewaretoken : csrftoken,
+                            activity : "refresh comments",
+                        },
+                        complete : function(response) {
+                            // disply notify msg to the user
+                            $indicator.toggleClass("notif-active");
+                            $success.text("Comment successfully posted!");
+                            $success.removeClass("hidden");
+                            setTimeout(function(){
+                                $indicator.removeClass("notif-active");
+                                $success.text("");
+                                $success.toggleClass("hidden");
+                            }, 2000);
+                            $comment_body.css({"opacity":"1", "pointer-events":"all"});
+                            $comment_send_form.css("pointer-events", "all");
+                            $post_comment_wrapper.html(response.responseText);
+                        },
+                    });
+                } else {
+                    // disply notify msg to the user
+                    $indicator.toggleClass("notif-active");
+                    $error.text("Post unsuccessful, Try again!");
+                    $error.removeClass("hidden");
+                    setTimeout(function(){
+                        $indicator.removeClass("notif-active");
+                        $error.text("");
+                        $error.toggleClass("hidden");
+                    }, 3000);
+                    $comment_body.css({"opacity":"1", "pointer-events":"all"});
+                    $comment_send_form.css("pointer-events", "all");
+               }
+            }
+        });
+    });
+});
