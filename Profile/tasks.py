730fa83bc8fe62041310e4ae49bdd6d9776044cb
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-08-04 23:27:30 +0530

    added new task manage_likes

--- Profile/tasks.py
+++ Profile/tasks.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from Profile.models import User, Account_Notif_Settings
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import F
+from Profile.models import User, Account_Notif_Settings
+from Home.tasks import send_notifications, del_notifications
+from Home.models import PostModel, PostLikes
 
 @shared_task
 def update_user_acc_settings(username, data):
@@ -31,3 +34,25 @@ def update_user_acc_settings(username, data):
     except:
         return 'User account settings was not successfully updated :('
 
+@shared_task
+def manage_likes(post_id, username):
+    try:
+        post = PostModel.objects.get_post(post_id=post_id)
+        user = User.get_user_obj(username=username)
+        if post.post_like_obj.filter(user=user).exists():
+            # Dislike post
+            post.post_like_obj.filter(user=user).delete()
+            del_notifications.delay(username=user.username, reaction="Disliked", send_to_username=post.user.username, post_id=post_id)
+            post.likes_count = F('likes_count') - 1
+            post.save()
+            return f'post - {post_id} was disliked by {username}'
+        else:
+            # Like post
+            post.post_like_obj.add(PostLikes.objects.create(post_obj=post, user=user))
+            post.likes_count = F('likes_count') + 1
+            post.save()
+            # Notify the user whose post is being liked
+            send_notifications.delay(username=user.username, reaction="Liked", send_to_username=post.user.username, post_id=post_id)
+            return f'post - {post_id} was liked by {username}'
+    except:
+        return 'task failed'
