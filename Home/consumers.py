d8c628461915a297bf91885bcf9b44eb6ca26c89
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-08-08 22:10:07 +0530

    added support for comment-post-from-wall-upload

--- Home/consumers.py
+++ Home/consumers.py
@@ -2,7 +2,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.db.models import F
 from Profile.models import User
-from Home.models import PostModel, PostLikes
+from Home.models import PostModel, PostLikes, PostComments
 from Home.tasks import send_notifications, del_notifications
 import json
 
@@ -17,14 +17,34 @@ class CentralPerkHomeConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data_from_client = json.loads(text_data)
         if data_from_client.get('task') is not None:
+            # like/Dislike posts from wall-posts
             if data_from_client['task'] == 'post_like':
-                post_id = data_from_client['post_id']
+                post_id = data_from_client.get('post_id', None)
                 response = await self.like_post_from_wall(post_id=post_id)
                 await self.send(text_data=json.dumps({
                     'type' : 'likes_count',
                     'post_id' : post_id,
                     'count' : response,
                 }))
+            # Post comments from wall-posts
+            elif data_from_client['task'] == 'post_comment':
+                post_id = data_from_client.get('post_id', None)
+                comment = data_from_client.get('comment', None)
+                response = await self.post_comment_from_wall(post_id=post_id, comment=comment)
+                if response == 'comment cannot be empty' or response is None:
+                    await self.send(text_data=json.dumps({
+                        'type' : 'error',
+                        'error' : response,
+                    }))
+                else:
+                    await self.send(text_data=json.dumps({
+                        'type' : 'comment_count',
+                        'post_id' : post_id,
+                        'count' : response,
+                    }))
+            else:
+                pass
+
     
     @database_sync_to_async
     def like_post_from_wall(self, post_id):
@@ -50,4 +70,20 @@ class CentralPerkHomeConsumer(AsyncWebsocketConsumer):
         except:
             pass
 
-    
+    @database_sync_to_async
+    def post_comment_from_wall(self, post_id, comment):
+        user = self.scope['user']
+        if comment.isspace() or not len(comment):
+            # Cannot accept blank comments or comments with only spaces or newlines
+            return 'comment cannot be empty'
+        try:
+            post_obj = PostModel.objects.get_post(post_id=post_id)
+            post_obj.post_comment_obj.add(PostComments.objects.create(user=user, post_obj=post_obj, comment=comment))
+            post_obj.comment_count = F('comment_count') + 1
+            post_obj.save()
+            post_obj.refresh_from_db()
+            # Notify the user whose post you commented on
+            send_notifications.delay(username=user.username, reaction='Commented', send_to_username=post_obj.user.username, post_id=post_id)
+            return post_obj.comment_count
+        except:
+            pass
