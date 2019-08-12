cd19d416df36e7cd14300dce0cb73b4de9355795
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-08-12 16:41:04 +0530

    added update_notif signal on model dlt & save

---
+++ Home/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from Home.models import UserNotification
+from Profile.models import User
+
+@receiver([post_save, post_delete], sender=UserNotification)
+def update_notifications(sender, instance, **kwargs):
+    print("signal received!")
+    channel_name = instance.user_to_notify.channel_name
+    if channel_name is not "":
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.send)(
+            channel_name, {
+                "type" : "send.updated.notif",
+            }
+        )
