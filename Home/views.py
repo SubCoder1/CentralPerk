03af476ae93bd55aae6dd94a41d7b0f2172329a2
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-09-16 22:15:17 +0530

    pic_thumbnail is compressed to size and saved in model

--- Home/views.py
+++ Home/views.py
@@ -3,13 +3,16 @@ from django.views.generic import TemplateView
 from django.db.models import F, Prefetch
 from django.urls import reverse
 from django.contrib import messages
+from django.core.files import File
 from django.core.exceptions import ObjectDoesNotExist
 from Home.models import PostModel, PostLikes, PostComments, UserNotification
 from Home.forms import PostForm
 from Profile.models import Friends, User
 from Home.tasks import share_posts, send_notifications, del_notifications
+from io import BytesIO
 from datetime import datetime
-import pytz, uuid
+import pytz, uuid, os
+from PIL import Image
 # Create your views here.
 
 class home_view(TemplateView):
@@ -32,9 +35,33 @@ class home_view(TemplateView):
             post = form.save(commit=False)
             post.user = request.user
             post.post_id = str(post.unique_id)[:8]
+            if post.pic:
+                pic = Image.open(post.pic)
+
+                if pic.format is not 'GIF':
+                    # create a BytesIO object
+                    im_io = BytesIO()
+                    # Compress image into thumbnail
+                    if pic.format is 'PNG':
+                        pic = pic.convert('RGB')
+                    
+                    size = (300, 300)
+                    pic.thumbnail(size, Image.ANTIALIAS)
+                    # save image to BytesIO object
+                    pic.save(im_io, format='JPEG', quality=95, optimize=True)
+                    # Now save the thumbnail into model's pic_thumbnail
+                    # create a django-friendly Files object
+                    post.pic_thumbnail = File(im_io, name=f"thumb_{str(post.unique_id)}.jpg")
             post.save()
+
+            """if post.pic:
+                # Compress and save pic thumbnail
+                pic = Image.open(post.pic.path)
+            """
+
             post.send_to.add(request.user)
-            share_posts.delay(request.user.username, post.post_id)  # Celery handling the task to share the post to user's followers
+            # Celery handling the task to share the post to user's followers
+            share_posts.delay(request.user.username, post.post_id)
             messages.success(request, 'Post Successful!')
         else:
             messages.error(request, 'Post Unsuccessful!')
