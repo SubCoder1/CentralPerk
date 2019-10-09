7fc0c9bf1235a41e187c69be4b0a03a84f8d2668
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-10-10 02:47:42 +0530

    added secret_key to envior var set in heroku

--- centralperk/settings.py
+++ centralperk/settings.py
@@ -12,7 +12,7 @@ SESSION_SECURITY_EXPIRE_AFTER = 100 # Time (in seconds) before the user should b
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'yoursecretkey'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
