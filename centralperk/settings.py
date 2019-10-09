5ddea39de1647db52748a417c98c488a9f09d9a0
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-10-10 01:36:31 +0530

    changed debug to False

--- centralperk/settings.py
+++ centralperk/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yoursecretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',]
 INTERNAL_IPS = ['127.0.0.1',]
