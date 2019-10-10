ea0ec88ea557f1253a83ea9192a33aab700ecb1f
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-10-10 14:23:57 +0530

    fixed not-null issue in username

--- Profile/models.py
+++ Profile/models.py
@@ -16,7 +16,7 @@ class User(AbstractBaseUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     session_key = models.CharField(max_length=40, default='notyetaccquired')
     channel_name = models.CharField(max_length=100, default="", blank=True, null=True)
-    username = models.CharField(max_length=20, unique=True)
+    username = models.CharField(max_length=20, default='',unique=True)
     full_name = models.CharField(max_length=50)
     birthdate = models.CharField(max_length=10)
     bio = models.TextField(max_length=160,  default="I am breathtaking!", blank=True)
