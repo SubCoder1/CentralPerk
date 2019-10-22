14ce9693ef373b54494baa2ed0d60fea0fd7f6f8
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-10-23 02:55:32 +0530

    fixed bugs in signup view

--- AUth/views.py
+++ AUth/views.py
@@ -81,10 +81,10 @@ def register_user(request):
                     login(request, user)
                     Account_Settings.objects.create(user=user)
                     Friends.objects.create(current_user=user)
-                    default_img = Image.open(settings.STATIC_ROOT + '/signup/img/default.jpeg')
+                    default_img = Image.open(settings.STATIC_ROOT + '/signup/img/default.png')
                     im_io = BytesIO()
-                    default_img.save(im_io, format='JPEG', quality=90, optimize=True)
-                    user.profile_pic = File(im_io, name=f"thumb_{str(user.user_id)}.jpeg")
+                    default_img.save(im_io, format='PNG', quality=90, optimize=True)
+                    user.profile_pic = File(im_io, name=f"thumb_{str(user.user_id)}.png")
                     user.save()
                     result = 'valid form'
             else:
