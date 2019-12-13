73eb5ff8507d14e179254b9bca6d8093428a04e6
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-12-14 02:35:25 +0530

    added postgres as local db

--- centralperk/settings.py
+++ centralperk/settings.py
@@ -106,8 +106,12 @@ PASSWORD_HASHERS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'centralperk',
+    "USER": 'postgres',
+    "PASSWORD": 'admin',
+    "HOST": 'localhost',
+    "PORT": '5432',
     }
 }
 
