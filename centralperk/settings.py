5b01486ab5f8ef4d8c6cd5a98b80f22112c309b4
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-12-15 01:45:05 +0530

    rolled back settings.py

--- centralperk/settings.py
+++ centralperk/settings.py
@@ -98,12 +98,8 @@ PASSWORD_HASHERS = (
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'centralperk',
-    "USER": 'postgres',
-    "PASSWORD": 'admin',
-    "HOST": 'localhost',
-    "PORT": '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
