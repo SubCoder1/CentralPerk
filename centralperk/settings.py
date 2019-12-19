cb9b681d2bdb393105f558438b9246aff82bc9a6
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-12-20 02:45:33 +0530

    update session expiry ETA to 8mins

--- centralperk/settings.py
+++ centralperk/settings.py
@@ -3,7 +3,7 @@ import os
 #CSRF_COOKIE_DOMAIN = None
 #CSRF_COOKIE_SECURE = False
 #SESSION_COOKIE_SECURE = False
-SESSION_COOKIE_AGE = 240
+SESSION_COOKIE_AGE = 480
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True # If a logged-in user closes the browser, session gets expired
 SESSION_SAVE_EVERY_REQUEST = True # whenever you occur new request, It saves the session and updates timeout to expire
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
